b12c8230c78eddf748cb375012c4ed621cae3ee1
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-29 13:47:12 +0900

    각 네이티브 폰트 세팅 및 텍스트 컴포넌트 추가

---
+++ speeditApp/src/styles/colors.ts
@@ -0,0 +1,16 @@
+// #E8E8E8 = rgb(232,232,232)
+
+export const textColor = {
+  100: 'rgba(232,232,232,1)',
+  80: 'rgba(232,232,232,0.8 )',
+  50: 'rgba(232,232,232,0.5)',
+  30: 'rgba(232,232,232,0.3)',
+  20: 'rgba(232,232,232,0.2)',
+  5: 'rgba(232,232,232,0.05)',
+  primaryRed: '#F86565',
+  primaryGreen: '#5FD997',
+};
+
+export const bgColor = {
+  primaryBlack: '#1C1C1E',
+};
