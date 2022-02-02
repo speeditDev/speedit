9b2a53f54c35368e0dfdcae47c8fc17760fbfd9a
오진석 <freejin4802@gmail.com>
2022-02-02 17:46:24 +0900

    Re initialize
    webflux 와 spring data jdbc 와의 호환이 좋지않음에따라
    spring web / spring data jpa로 변경

--- server/src/main/kotlin/com/speedit/api/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.speedit.api.repository
-
-import com.speedit.api.model.User
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository: CrudRepository<User, Int> {
-    
-}
--- server/src/main/kotlin/com/speedit/api/service/impl/AuthService.kt
+++
@@ -1,8 +0,0 @@
-package com.speedit.api.service.impl
-
-import com.speedit.api.service.IAuthService
-import org.springframework.stereotype.Service
-
-@Service
-class AuthService: IAuthService {
-}
